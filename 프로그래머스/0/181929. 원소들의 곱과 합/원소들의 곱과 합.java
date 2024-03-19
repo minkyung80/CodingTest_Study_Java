23c4cc6627dc90cf61e6626b381d3a6372fcadac
minkyung <150425341+minkyung80@users.noreply.github.com>
2024-03-19 18:49:46 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int add = 0;
+        int multiply = 1;
+        
+        for(int i = 0; i< num_list.length; i++){
+            add += num_list[i];
+            multiply *= num_list[i];
+        }
+        
+        if((add*add) > multiply){
+            return 1;
+        }
+        
+        return 0;
+    }
+}
