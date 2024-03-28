037a8e42686491bc1eeea21f8615b7b36ebaf242
minkyung <150425341+minkyung80@users.noreply.github.com>
2024-03-28 23:58:19 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int mul = 1;
+        
+        for(int i = 0; i < num_list.length; i++){
+
+            if(11 <= num_list.length){
+                sum += num_list[i];
+                answer = sum;
+    
+            }
+            else{
+                mul *= num_list[i];
+                answer = mul;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
