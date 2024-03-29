73508320f91501b9a542bdaae0642c0c72876cfc
minkyung <150425341+minkyung80@users.noreply.github.com>
2024-03-29 17:19:11 +0900

    [level 0] Title: qr code, Time: 13.15 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(int q, int r, String code) {
         String answer = "";
-        char charCode;
         
         for(int i =0; i < code.length(); i++){
             if(i % q == r){
